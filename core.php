22cd88fe5e375e69d68ed5d5a74666cfc69acd8c
sahilmenrai <53208259+sahilmenrai@users.noreply.github.com>
2019-07-23 15:21:53 +0930

    website

---
+++ core.php
@@ -0,0 +1,217 @@
+<!doctype html>
+<html>
+<head>
+<title>Dagama Pereira Immigration Lawyers</title>
+	<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all"/>
+	<!-- fontawesome -->
+	<link href="css/animate.min.css" type="text/css" rel="stylesheet">
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all"/>
+	<!-- Bootstrap stylesheet -->
+	<link href="css/styles.css" rel="stylesheet" type="text/css" media="all"/>
+	<link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css"/>
+	<link href="css/lightbox.min.css" type="text/css" rel="stylesheet"/>
+
+	<!-- stylesheet -->
+	<!-- meta tags -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	
+	<!-- //meta tags -->
+	<!--fonts-->
+	<link href="//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i&subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Vollkorn+SC:600" rel="stylesheet">
+
+	<!--//fonts-->
+	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+	<!-- Required-js -->
+	<script src="js/parallax.min.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<script src="js/bootstrap-datepicker.min.js"></script>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/lightbox.min.js"></script>
+	<script type="text/javascript">	</script>
+	
+	</head>
+
+<body>
+
+<div class="navbar navbar-default" style="width: 100%" >
+	<div class="container">
+		<a href="" class="navbar-brand"><img src="images/logo1.png" height="30px;" width="41%"></a>
+		<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+			
+		</button>
+		
+		<div class="collapse navbar-collapse navHeaderCollapse fixed-top">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="index.php">Home</a></li>
+			<li class="dropdown"><a href="aboutus.php" class="dropdown-toggle" data-toggle="dropdown">About<b class="caret"></b></a>
+			<ul class="dropdown-menu">
+			<li><a href="aboutus.php">About Us</a></li>
+			<li><a href="core.php">Core Values</a></li>
+			</ul>
+			</li>
+			
+			<li><a href="#">Australian Visa</a></li>
+			<li><a href="#">Consultation</a></li>
+			<li><a href="#">Useful Information</a></li>
+			<li><a href="#">Contact Us</a></li>
+			
+			
+			
+				
+			</ul>
+		</div>
+		
+		
+	</div>
+	
+</div>
+
+
+
+
+<div class="section" data-parallax="scroll" style="background-image: linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6))" data-image-src="images/consultant.jpg"  >
+<div class="container" style="margin-top: -31px;">
+	<div class="services animated fadeInDown">
+		<h1>Core Values</h1>
+		<p>Our team shares a set of core values which forms the basis of our organisation and Getting the outcome for the client is of primary importance to us and entails the following qualities:</p>
+	</div>
+	<div class="row">
+		<div class="col-sm-3 text-center h animated slideInLeft">
+			<div class="icon"><i class="fa fa-gavel"></i></div>
+			<h3>Outcome focused</h3>
+			<p>Cost-effective and quickest solution</p>
+		</div>
+		<div class="col-sm-3 text-center h animated slideInLeft">
+			<div class="icon"><i class="fa fa-angellist"></i></div>
+			<h3>Passionate</h3>
+			<p>Staff are fully engaged at all times.</p>
+		</div>
+		<div class="col-sm-3 text-center h animated slideInRight">
+			<div class="icon"><i class="fa fa-book"></i></div>
+			<h3>Determination </h3>
+			<p>Some cases end up being complex , but Dagama Pereira Immigration Lawyers continues to services a client with Strong legal skills.</p>
+		</div>
+		<div class="col-sm-3 text-center h animated slideInRight">
+			<div class="icon"><i class="fa fa-arrows-alt"></i></div>
+			<h3>Alternate Options</h3>
+			<p> We provide our client with options based on their brief, documentation and instructions.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-3 text-center h animated slideInLeft">
+			<div class="icon"><i class="fa fa-code"></i></div>
+			<h3>Technical Excellence </h3>
+			<p>As a Law firm with an Accredited Specialist we have a strong understanding of Migration Laws and Policy, when providing Legal Advice.</p>
+		</div>
+		<div class="col-sm-3 text-center h animated slideInLeft">
+			<div class="icon"><i class="fa fa-check"></i></div>
+			<h3>Attention to Detail</h3>
+			<p> We are pedantic in our approach to analysis, service and delivery.</p>
+		</div>
+		<div class="col-sm-3 text-center h animated slideInRight">
+			<div class="icon"><i class="fa fa-contao"></i></div>
+			<h3>Consistency </h3>
+			<p>Consistency in quality service for the duration of the client’s involvement with us is paramount.</p>
+		</div>
+		<div class="col-sm-3 text-center h animated slideInRight">
+			<div class="icon"><i class="fa fa-globe"></i></div>
+			<h3>Ethical Approach  </h3>
+			<p>Ethics is our core value in all our dealings – with our clients and the Department of Immigration.</p>
+		</div>
+	</div>
+</div>
+</div>
+<div class="container">
+	<div class="row" >
+		<h1 style="color: black; font-size: 26px; font-family: 'aguafina-script'">Innovation</h1>
+	</div>
+	
+	<div class="container-fluid" >
+	<div class="row">
+		<div class="col-sm-2">
+			<img src="images/annual.jpg" style="height: 150px; width: 160px;">
+		</div>
+		<div class="col-sm-4">
+			<p style="color: black; font-size: 18px;">Immigration laws is one of the most complex laws that change frequently and we analyse every change from the time it is introduced into parliament and/or made.</p>
+		</div>
+		
+	
+	
+		<div class="col-sm-2">
+			<img src="images/annual.jpg" style="height: 150px; width: 160px;">
+		</div>
+		<div class="col-sm-4">
+			<p style="color: black; font-size: 18px;">Quality employees are a precious resource for any companys.</p>
+		</div>
+		</div></div>
+	</div>
+
+  <!-- Footer -->
+        <footer style="margin-top: 10px;">
+        	<div class="footer-top">
+		        <div class="container">
+		        	<div class="row">
+		        		<div class="col-md-3 footer-about wow fadeInUp">
+		        			<img class="logo-footer" src="images/logo1.png" alt="logo-footer" data-at2x="images/logo1.png" style="width: 200px; height: 120px">
+		        			
+	                    </div>
+		        		<div class="col-md-4 offset-md-1 footer-contact wow fadeInDown">
+		        			<h3>Contact</h3>
+		                	<p><i class="fa fa-map-marker"></i> 7/230 Church Street Richmond Victoria 3121</p>
+		                	<p><i class="fa fa-phone"></i> Phone: 03 94281198</p>
+		                	<p><i class="fa fa-fax"></i>Fax: 03 94290819 </a></p>
+		                	<p><i class="fa fa-skype"></i> Skype: dagama.pereirausa</p>
+	                    </div>
+	                    <div class="col-md-4 footer-links wow fadeInUp">
+	                    	<div class="row">
+	                    		<div class="col">
+	                    			<h3>Useful Links</h3>
+	                    		</div>
+	                    	</div>
+	                    	<div class="row">
+	                    		<div class="col-md-6">
+	                    			<p><a class="scroll-link" href="#">Home</a></p>
+	                    			<p><a href="#">About Us</a></p>
+	                    			<p><a href="#">Our Core Values</a></p>
+	                    			<p><a href="#">Contact Us</a></p>
+	                    		</div>
+	                    		<div class="col-md-6">
+	                    			<p><a href="#">Fees Payment & pricing</a></p>
+	                    			<p><a href="#">Business Visa</a></p>
+	                    			<p><a href="#">Sponsoring Employees</a></p>
+	                    			<p><a href="#">Important Links</a></p>
+	                    		</div>
+	                    	</div>
+	                    </div>
+		            </div>
+		        </div>
+	        </div>
+	        <div class="footer-bottom">
+	        	<div class="container">
+	        		<div class="row">
+	           			<div class="col-md-6 footer-copyright">
+	                    	&copy; COPYRIGHT DAGAMA PEREIRA AND ASSOCIATES PTY LTD 2016. ALL RIGHTS RESERVED. | PRIVACYPOLICY
+ 
+	                    </div>
+	           			<div class="col-md-6 footer-social">
+	                    	<a href="#"><i class="fa fa-facebook-f"></i></a> 
+							<a href="#"><i class="fa fa-twitter"></i></a> 
+							<a href="#"><i class="fa fa-instagram"></i></a> 
+							
+	                    </div>
+	           		</div>
+	        	</div>
+	        </div>
+        </footer>
+
+
+
+</body>
+</html>
